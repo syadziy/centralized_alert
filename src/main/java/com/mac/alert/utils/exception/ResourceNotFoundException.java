c43720ddf317e173d8877fd21dab0cef50959f88
Syadziy's Mac <mymac@syadziy.local>
2026-08-07 20:04:17 +0700

    update move global exception to sdk

--- src/main/java/com/mac/alert/utils/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mac.alert.utils.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
